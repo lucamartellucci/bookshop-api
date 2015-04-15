ded2059fcc4830277db5aaf050f026e6d29dc445
luca <luca.martellucci@absontheweb.com>
2015-04-15 19:16:52 +0200

    Switched test db to H2

--- src/main/db-config/mysql/BOOKSHOP_MYSQL_TEST.sql
+++
@@ -1,8 +0,0 @@
-/* creazione del database */
-CREATE DATABASE IF NOT EXISTS `bookshop_test` DEFAULT CHARACTER SET latin1;
-
-
-/* creazione utente applicativo*/
-CREATE USER 'bookshop_test'@'localhost' IDENTIFIED BY 'bookshop';
-
-GRANT ALL PRIVILEGES ON bookshop_test.* TO 'bookshop_test'@'localhost';
