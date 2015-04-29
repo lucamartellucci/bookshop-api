210bcb17485890b7e4674ad81eaf5f16e148c367
luca <luca.martellucci@absontheweb.com>
2015-04-29 19:52:23 +0200

    Bugfix custom exception handler

--- src/main/java/com/absontheweb/bookshop/web/controller/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/absontheweb/bookshop/web/controller/exception/RestResponseEntityExceptionHandler.java
@@ -1,24 +1,28 @@
 package com.absontheweb.bookshop.web.controller.exception;
 
+import java.io.IOException;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.absontheweb.bookshop.model.ErrorBuilder;
 import com.absontheweb.bookshop.model.Error;
+import com.absontheweb.bookshop.model.ErrorBuilder;
 
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler {
 
+	@ExceptionHandler (value = { ResourceNotFoundException.class })
     @ResponseStatus (HttpStatus.NOT_FOUND)
-    @ExceptionHandler (value = { ResourceNotFoundException.class })
     protected @ResponseBody Error handleAccessDeniedException( Exception ex, WebRequest request ) {
         return ErrorBuilder.error()
         		.withCode(ErrorCode.RESOURCE_NOT_FOUND)
@@ -26,16 +30,38 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     }
 
 
-    @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler (value = { Exception.class, UndeclaredThrowableException.class })
+    @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
     protected @ResponseBody Error handleGenericExceptions( Exception ex, WebRequest request ) {
         return new Error( ErrorCode.GENERIC_ERROR, ex.getMessage() );
     }
     
-    @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler (value = { InternalServerErrorException.class })
+    @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
     protected @ResponseBody Error handleInternalServerErrorExceptions( Exception ex, WebRequest request ) {
     	return new Error( ErrorCode.GENERIC_ERROR, ex.getCause().getMessage() );
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public @ResponseBody Map<String, Object> handleValidationException(MethodArgumentNotValidException ex) throws IOException {
+        Map<String, Object>  map = new HashMap<>();
+        map.put("error", "Validation Failure");
+        map.put("violations", convertConstraintViolation(ex));
+        return map;
+    }
+    
+ 
+    private Map<String, Map<String, Object> > convertConstraintViolation(MethodArgumentNotValidException ex) {
+        Map<String, Map<String, Object> > result = new HashMap<>();
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            Map<String, Object>  violationMap = new HashMap<>();
+            violationMap.put("target", ex.getBindingResult().getTarget());
+            violationMap.put("type", ex.getBindingResult().getTarget().getClass());
+            violationMap.put("message", error.getDefaultMessage());
+            result.put(error.getObjectName(), violationMap);
+        }
+        return result;
     }
 
 }
