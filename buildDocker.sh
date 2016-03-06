c024c0319899908b35208579bc37a94a95f0c639
luca <luca.martellucci@absontheweb.com>
2016-03-06 22:21:55 +0100

    update docker setup

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
-echo "Building application"
-#mvn clean package -DskipTest=true
+# env setup
+DOCKER_BUILD_DIR=target-docker
+DOCKER_SRC_DIR=docker
+JAVA_TARGET_DIR=target
+MIDDLEWARE_IMAGE_NAME="abs/bookshop-middleware"
 
-mkdir -p docker/middleware/files
-cp target/bookshop-0.0.1-SNAPSHOT.jar docker/middleware/files
 
-echo "Building a set of docker images in order to run the Bookshop Application"
+echo "Building book-service application"
+mvn clean package -DskipTest=true
 
-docker build -t abs/bookshop-middleware docker/middleware/
+if [-d "$DOCKER_BUILD_DIR" ]; then
+ rm -rf $DOCKER_BUILD_DIR
+fi
 
-rm -rf docker/middleware/files
+mkdir $DOCKER_BUILD_DIR
+
+echo "START docker book-service image build"
+
+mkdir -p "$DOCKER_BUILD_DIR/middleware/files"
+cp "$DOCKER_SRC_DIR/middleware/Dockerfile" "$DOCKER_BUILD_DIR/middleware"
+cp "$JAVA_TARGET_DIR/bookshop-0.0.1-SNAPSHOT.jar"  "$DOCKER_BUILD_DIR/middleware/files"
+
+
+docker build -t $MIDDLEWARE_IMAGE_NAME "$DOCKER_BUILD_DIR/middleware"
+
 
 
 
