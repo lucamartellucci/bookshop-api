4d30d3157dd184711446aa864214a198dc430c5b
luca <luca.martellucci@absontheweb.com>
2016-03-13 17:32:14 +0100

    the spring logo is now logged on logfile instead of console

--- src/main/java/com/absontheweb/bookshop/application/Application.java
+++ src/main/java/com/absontheweb/bookshop/application/Application.java
@@ -1,5 +1,6 @@
 package com.absontheweb.bookshop.application;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+	    SpringApplication app = new SpringApplication(Application.class);
+	    app.setBannerMode(Banner.Mode.LOG);
+	    app.run(args);
     }
 
 }
