1a70c69799026fde1ec85dff0938322e1a7f72fc
Luca Martellucci <luca.martellucci@absontheweb.com>
2016-04-03 08:59:06 +0200

    Test for PDM unusedprivatefields suppress warning

--- src/main/java/com/absontheweb/bookshop/book/model/Author.java
+++ src/main/java/com/absontheweb/bookshop/book/model/Author.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.Data;
 
 @Data
+@SuppressWarnings("PMD.UnusedPrivateField")
 public class Author implements Serializable {
 
 	private static final long serialVersionUID = 1L;
